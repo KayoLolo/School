af70a0b08971bedd6e26cd80388acf4554d613d3
Karl <karlcreppy@gmail.com>
2024-10-10 16:25:52 +0200

    Update niv1.ts

--- TD/niv1.ts
+++ TD/niv1.ts
@@ -111,6 +111,22 @@
         console.log(pgcd(24,32))
         
         
+//i.
+
+function is_perfect(n:number):boolean{
+    let res:number=0
+    for(let i=1; i<n; i++){
+        if(n%i==0){
+            res+=i
+        }
+        if(res==n){
+            return true
+        }
+    }
+    return false
+}
+console.log(is_perfect(6))
+console.log(is_perfect(7))
         
         
         
